42cd498b38f8462e605cba8379bbd76ef6a2b9a3
AirysDark <47623717+AirysDark@users.noreply.github.com>
2025-09-16 22:37:20 +1000

    Update esp32_marauder_rewired.ino

--- scaffold/esp32_marauder_rewired.ino
+++ scaffold/esp32_marauder_rewired.ino
@@ -1,17 +1,28 @@
 #include <Arduino.h>
-#include "menus/MenuFromRegistry.h"
+#include <CustomFunctions.h>   // <-- custom helpers (from your zipped libs)
+
+#include "menus/MenuTypes.h"
 #include "menus/ToolRegistry.h"
 #include "menus/RegistryInit.h"
-#Include <CustomFunctions.h>
+#include "menus/MenuFromRegistry.h"
 
 void setup() {
   Serial.begin(115200);
   delay(100);
+
+  // If your CustomFunctions library has an initializer, uncomment:
+  // Custom::init();
+
   init_tool_registry();
   showMainMenu();
 }
 
 void loop() {
-  // Minimal idle loop for now
+  // If your custom lib expects serial commands, you can forward them:
+  // if (Serial.available()) {
+  //   String cmd = Serial.readStringUntil('\n');
+  //   Custom::handleCommand(cmd);
+  // }
+
   delay(10);
 }
